2016416faa23cf70829adf50f428311e9daeab2b
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2022-10-28 14:32:51 +0800

    Lance 10/28 imgGallery

--- lance.php
+++ lance.php
@@ -4,9 +4,218 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="lance.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Blog Gallery</title>
 </head>
 <body>
-    
+
+<nav class="navbar navbar-expand-sm navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="img/CabalenLight.png" alt="" width="120" height="72">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Places</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blogs</a>
+        </li>    
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/CandabaSwamp.png" alt="Candaba Swamp" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Mt.Arayat.png" alt="Mt. Arayat" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/Pinatubo.png" alt="Pinatubo" style="width:100%">
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/Betute.png" alt="Betute" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Bringhe.png" alt="Bringhe" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/Murcon.png" alt="Murcon" style="width:100%">
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/SanNicolasCookies.png" alt="San Nicolas Cookies" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Sisig.png" alt="Sisig" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/TibukTibok.png" alt="Tibuk-Tibok" style="width:100%">
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/KuranAmpongPasu.png" alt="Kuran Ampong Pasu" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Pukpuk.png" alt="Pukpuk" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/parul.png" alt="Parul" style="width:100%">
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/Burarul.png" alt="Burarul" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Dase.png" alt="Dase" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/Kupia.png" alt="Kupia" style="width:100%">
+      </a>
+    </div>
+  </div>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/Dukit.png" alt="Dukit" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Santos.png" alt="Santos" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/Gitara.png" alt="Gitara" style="width:100%">
+      </a>
+    </div>
+  </div>
+
+<div class="imgGallery p-5">
+<div class="row">
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/GawangPande.png" alt="Gawang Pande" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/GiantLantern.png" alt="Giant Lantern Festival" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/FiestangTugak.png" alt="Fiestang Tugak" style="width:100%">
+      </a>
+    </div>
+  </div>
+
+</div>
+</div>
+
 </body>
 </html>
