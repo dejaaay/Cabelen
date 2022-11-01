5cf10de53a544ac9520f5e302b9f4938e42602d4
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2022-11-01 12:33:19 +0800

    video

--- index-peter.php
+++ index-peter.php
@@ -14,9 +14,9 @@
     <title>Cabalen</title>
 </head>
 <body>
-    
+
 <div class="container">   
-<iframe width="560" height="315" src="https://www.youtube.com/embed/-3k-SC22zbA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<video><iframe width="560" height="315" src="https://www.youtube.com/embed/-3k-SC22zbA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></video>
 </div> 
     
 </body>
