39df2cc91016ea873c509f2162f28fbc3692b372
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2022-11-01 12:39:38 +0800

    center

--- index-peter.php
+++ index-peter.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 
-<div class="container center">   
+<div class="container">   
 <iframe width="560" height="315" src="https://www.youtube.com/embed/-3k-SC22zbA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div> 
     
