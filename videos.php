910c25a6c7fbb5ad589a2b1c8bef7889ccca7e22
qlabrys <qlabrys@gmail.com>
2022-11-01 14:36:14 +0800

    11/01 - JM - Index update, bare videos page

---
+++ videos.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="style-jm.css" rel="stylesheet" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Content Section 1 -->
+    <section id="scroll">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><iframe width="560" height="315" src="https://www.youtube.com/embed/howgEFee5MU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <p>Learn more about the wonders of Pampanga: The Culinary Capital of the Philippines through this video by Shelley Casao.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <!-- Content Section 2 -->
+    <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><iframe width="560" height="315" src="https://www.youtube.com/embed/mcxMnVD26FU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <p>Take a quick look at the Sinukwan Festival, a weeklong festival that captures the soul of San Fernando City. <br><br>Best described as the essence of the Kapampangan Spirit, Sinukwan is a cultural festival, so you would expect that most of the events are steeped in the traditional culture of the natives. A festival of this magnitude in the Philippines is incomplete without the spectacular street dances that bring locals together in a remarkably fluid manner. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content Section 3 -->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><iframe width="560" height="315" src="https://www.youtube.com/embed/C8q1F1KHgXE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <p>Join GMA's Biyahero Drew as he explores internationally-inspired restaurants and attractions in Pampanga.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Content Section 4 -->
+        <section>
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5"><iframe width="560" height="315" src="https://www.youtube.com/embed/I2zykJTKel4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <p>Watch GMA's Biyahe ni Drew's coverage of the San Guillermo Parish Church in Bacolor, known for it being half-buried due to a lahar overflow during Mount Pinatubo's eruption.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+</body>
+</html>
