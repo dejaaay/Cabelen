9dd9b2205a8f5ad1cd87a9f19a4aa86b2c2d4b98
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2022-11-02 11:53:33 +0800

    alignment

--- index-peter.php
+++ index-peter.php
@@ -29,14 +29,14 @@
  <!--space here for Current news and updates grid-->
 <!--Video for about -->
 <iframe 
-class= "center"
-width="560" 
-height="315" 
-src="https://www.youtube.com/embed/tuO6oCxZU-s" 
-title="YouTube video player" 
-frameborder="0" 
-allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-allowfullscreen>
+    class= "center"
+    width="560" 
+    height="315" 
+    src="https://www.youtube.com/embed/tuO6oCxZU-s" 
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
 </iframe>
 <!--news and blogs-->
 <h1> News and blogs</h1>
