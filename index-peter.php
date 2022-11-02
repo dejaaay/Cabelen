f7ec26800f6785647e25f0197e44b451d8ff9d0f
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2022-11-02 13:37:57 +0800

    try

--- index-peter.php
+++ index-peter.php
@@ -47,9 +47,11 @@
             <div class="card" style="width: 18rem;">
                <img src="https://www.karlaroundtheworld.com/wp-content/uploads/2019/12/Pampanga-141-of-177-scaled.jpg" class="card-img-top" alt="pampanga art">      
                <div class="card-body">
-                <h5 class="card-title">Kapampangan Art</h5>
-                <p class="card-text">There is no doubt that Pampanga has a very rich culture and tradition. There are some of these cultures that are famous not only in Pampanga but in the whole country</p>
-                <a href="https://kapampangan.org/is-there-kapampangan-art/" class="btn btn-primary">View article</a>
+                    <h5 class="card-title">Kapampangan Art</h5>
+                    <p class="card-text">There is no doubt that Pampanga has a very rich culture and tradition. There are some of these cultures that are famous not only in Pampanga but in the whole country</p>
+                    <div class="card-footer">
+                        <a href="https://kapampangan.org/is-there-kapampangan-art/" class="btn btn-primary">View article</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -77,7 +79,7 @@
         </div>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="" class="card-img-top" alt="...">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8ILksrzAf9dAmmTf3YffN1hb_aned-Mr20w&usqp=CAU" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Agencies urged: Take measures to prevent landslides at Mount Arayat</h5>
                     <p class="card-text">Gonzales over the weekend met with officials of the two agencies in preparation for the onslaught of tropical storm "Paeng" and tropical depression “Queenie”.</p>
