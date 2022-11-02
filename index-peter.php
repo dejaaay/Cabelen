347084af09ad8d89235740aa71544d3c05aa1861
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2022-11-02 11:54:12 +0800

    try

--- index-peter.php
+++ index-peter.php
@@ -18,8 +18,8 @@
 <!--greetings part below-->
 <iframe 
     class= "center"
-    width="560" 
-    height="315" 
+    width="860" 
+    height="515" 
     src="https://www.youtube.com/embed/-3k-SC22zbA?autoplay=1" 
     title="YouTube video player" 
     frameborder="0" 
