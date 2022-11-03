603e02fc7074c12a316d34a96a820bb70fe1b3cb
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2022-11-03 16:01:05 +0800

    Lance 11/3 Gallery Update

--- gallery-lance.php
+++ gallery-lance.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
     
     <link href="gallery-lance.css" rel="stylesheet">
+    <link href="gallery_newsletter.css" rel="stylesheet">
     <title>Blog Gallery</title>
 </head>
 <body>
@@ -18,7 +19,7 @@
 <nav class="navbar navbar-expand-sm navbar-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-        <img src="img/CabalenLight.png" alt="" width="120" height="72">
+        <img src="img/CabalenLight.png" alt="Cabalen Logo" width="120" height="72">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
@@ -39,179 +40,228 @@
   </div>
 </nav>
 
-<div class="imgGallery p-5">
+<div class="imgGallery p-2">
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/CandabaSwamp.png">
         <img src="img/CandabaSwamp.png" alt="Candaba Swamp" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Mt.Arayat.png">
         <img src="img/Mt.Arayat.png" alt="Mt. Arayat" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Pinatubo.png">
         <img src="img/Pinatubo.png" alt="Pinatubo" style="width:100%">
       </a>
     </div>
   </div>
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/AquaBeachClub.png">
+        <img src="img/AquaBeachClub.png" alt="Aqua Beach Club" style="width:100%">
+      </a>
+    </div>
+  </div>
 </div>
 </div>
 
-<div class="imgGallery p-5">
+<div class="imgGallery p-1">
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3 pl-2">
+    <div class="thumbnail">
+      <a href="img/AquaPlanet.png">
+        <img src="img/AquaPlanet.png" alt="Aqua Planet" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3 pl-2">
+    <div class="thumbnail">
+      <a href="img/DinosaursIsland.png">
+        <img src="img/DinosaursIsland.png" alt="Dinosaurs Island" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3 pl-2">
+    <div class="thumbnail">
+      <a href="img/MuseumOfKapampanganArts.png">
+        <img src="img/MuseumOfKapampanganArts.png" alt="Museum Of Kapampangan Arts" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3 pl-2">
+    <div class="thumbnail">
+      <a href="img/PuningSpring.png">
+        <img src="img/PuningSpring.png" alt="PuningSpring" style="width:100%">
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="imgGallery p-1">
+<div class="row">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/CandabaSwamp.png">
-        <img src="img/Betute.png" alt="Betute" style="width:100%">
+        <img src="img/Betute2.png" alt="Betute" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Mt.Arayat.png">
-        <img src="img/Bringhe.png" alt="Bringhe" style="width:100%">
+        <img src="img/Bringhe2.png" alt="Bringhe" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Pinatubo.png">
-        <img src="img/Murcon.png" alt="Murcon" style="width:100%">
+        <img src="img/Murcon2.png" alt="Murcon" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/PakoSalad.png" alt="Pako Salad" style="width:100%">
       </a>
     </div>
   </div>
 </div>
 </div>
 
-<div class="imgGallery p-5">
+<div class="imgGallery p-1">
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/CandabaSwamp.png">
-        <img src="img/SanNicolasCookies.png" alt="San Nicolas Cookies" style="width:100%">
-      </a>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="img/Mt.Arayat.png">
         <img src="img/Sisig.png" alt="Sisig" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/SanNicolasCookies.png" alt="San Nicolas Cookies" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Pinatubo.png">
         <img src="img/TibukTibok.png" alt="Tibuk-Tibok" style="width:100%">
       </a>
     </div>
   </div>
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/Tokwa_tBaboy.png" alt="Tokwa't Baboy" style="width:100%">
+      </a>
+    </div>
+  </div>
 </div>
 </div>
 
-<div class="imgGallery p-5">
+<div class="imgGallery p-1">
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/CandabaSwamp.png">
         <img src="img/KuranAmpongPasu.png" alt="Kuran Ampong Pasu" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Mt.Arayat.png">
         <img src="img/Pukpuk.png" alt="Pukpuk" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Pinatubo.png">
         <img src="img/parul.png" alt="Parul" style="width:100%">
       </a>
     </div>
   </div>
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/GawangPande2.png" alt="Gawang Pande" style="width:100%">
+      </a>
+    </div>
+  </div>
 </div>
 </div>
 
-<div class="imgGallery p-5">
+<div class="imgGallery p-1">
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/CandabaSwamp.png">
         <img src="img/Burarul.png" alt="Burarul" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Mt.Arayat.png">
         <img src="img/Dase.png" alt="Dase" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Pinatubo.png">
         <img src="img/Kupia.png" alt="Kupia" style="width:100%">
       </a>
     </div>
   </div>
-</div>
-</div>
-
-<div class="imgGallery p-5">
-<div class="row">
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="img/CandabaSwamp.png">
-        <img src="img/Dukit.png" alt="Dukit" style="width:100%">
-      </a>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="img/Mt.Arayat.png">
-        <img src="img/Santos.png" alt="Santos" style="width:100%">
-      </a>
-    </div>
-  </div>
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="img/Pinatubo.png">
-        <img src="img/Gitara.png" alt="Gitara" style="width:100%">
-      </a>
-    </div>
-  </div>
-</div>
-</div>
-
-<div class="imgGallery p-5">
-<div class="row">
-  <div class="col-md-4">
-    <div class="thumbnail">
-      <a href="img/CandabaSwamp.png">
-        <img src="img/GawangPande.png" alt="Gawang Pande" style="width:100%">
-      </a>
-    </div>
-  </div>
-  <div class="col-md-4">
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Mt.Arayat.png">
         <img src="img/GiantLantern.png" alt="Giant Lantern Festival" style="width:100%">
       </a>
     </div>
   </div>
-  <div class="col-md-4">
+</div>
+</div>
+
+<div class="imgGallery p-1 ">
+<div class="row">
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/CandabaSwamp.png">
+        <img src="img/Dukit.png" alt="Dukit" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/Mt.Arayat.png">
+        <img src="img/Santos.png" alt="Santos" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3">
+    <div class="thumbnail">
+      <a href="img/Pinatubo.png">
+        <img src="img/Gitara.png" alt="Gitara" style="width:100%">
+      </a>
+    </div>
+  </div>
+  <div class="col-md-3 pt-2 pb-3">
     <div class="thumbnail">
       <a href="img/Pinatubo.png">
         <img src="img/FiestangTugak.png" alt="Fiestang Tugak" style="width:100%">
@@ -223,7 +273,11 @@
 
 <footer class="footer">
   	 <div class="container">
+     
   	 	<div class="row">
+        <div class="footer-col">
+          <img src="img/CabalenLight.png" alt="Cabalen Logo" width="240" height="144" class="img">
+  	 		</div>
   	 		<div class="footer-col">
   	 			<h4>About Us</h4>
           <p style="color:white;">
@@ -250,8 +304,20 @@
   	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
   	 				<a href="#"><i class="fab fa-twitter"></i></a>
   	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
   	 			</div>
+          <br>
+          <div class="newsletter">
+            <h4>Subscribe to our Newsletter</h4>
+          </div>
+          
+          <form>
+            <div class="container">
+              <input type="text" placeholder="Enter Email address" name="mail" required>
+            </div>
+            <div class="container">
+              <input type="submit" value="Subscribe">
+            <div>
+          </form>     
   	 		</div>
   	 	</div>
   	 </div>
