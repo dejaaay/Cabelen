a3b8a39a7fbb0ae0d1f17fb309c45a8370613e87
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2022-11-18 08:12:21 +0800

    Lance 11/18 updates

---
+++ footer.php
@@ -0,0 +1,54 @@
+<footer class="footer">
+  	 <div class="container">
+     
+  	 	<div class="row">
+        <div class="footer-col">
+          <img src="img/CabalenLight.png" alt="Cabalen Logo" width="395" height="237" class="img">
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>About Us</h4>
+          <p style="color:white;">
+          Cabalen is a kapampangan word which means “Kabayan” in Tagalog. <br>
+          We chose this word because we treat every site visitor as our “Kabayan”. 
+          Our goal is to give you access to a Pampanga website that provides the best travel spots,
+          services, food, culture, and information at your fingertips 
+          to help you make the most of your vacation!
+          </p>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Blogs</h4>
+          <p style="color:white;">
+          Dive into different cultures, foods, and tourist destinations in Pampanga.
+          </p>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Pampanga</h4>
+           <p style="color:white;">
+           Pampanga is located in the central part of Region III. It is bounded on the north of Tarlac and Nueva Ecija, on the east by Bulacan, southwest by Bataan and west by Zambales. <br>
+           Its terrain is relatively flat with only one distinct mountain, Mount Arayat.
+          </p>
+          <br>
+          <h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="https://www.facebook.com/profile.php?id=100087116093597"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="https://twitter.com/CabalenEP"><i class="fab fa-twitter"></i></a>
+  	 				<a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+  	 		</div>
+          <br>
+          
+          <div class="newsletter">
+            <h4>Subscribe to our Newsletter</h4>
+          </div>
+          
+          <form>
+            <div class="container">
+              <input type="text" placeholder="Enter Email address" name="mail" required>
+            </div>
+            <div class="container">
+              <input type="submit" value="Subscribe">
+            <div>
+          </form>     
+  	 		</div>
+  	 	</div>
+  	 </div>
+</footer>
