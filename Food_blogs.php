8d7c18ebfab863ae08e5eabad2f3a127d716f39c
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2022-11-18 21:30:10 +0800

    11/18 lance+walt footer+blog

---
+++ Food_blogs.php
@@ -0,0 +1,222 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="blogs.css" rel="stylesheet">
+    <link href="gallery_newsletter.css" rel="stylesheet">
+    <link href="gallery.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container px-5" id="Food">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h1 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Food </h1> 
+            <img src="https://gttp.imgix.net/352610/x/0/" alt="Food" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Betute">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Betute </h2> 
+            <img src="https://kapampanganku.com/v3/wp-content/uploads/2019/08/Betute.jpg" alt="Betute" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Bringhe">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Bringhe </h2> 
+            <img src="https://i0.wp.com/www.angsarap.net/wp-content/uploads/2019/11/Bringhe-Wide.jpg?ssl=1" alt="Bringhe" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="Murcon">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Murcon </h2> 
+            <img src="https://outoftownblog.com/wp-content/uploads/2015/12/Morcon-photo-by-Delmonte.ph_-600x317.jpg" alt="Murcon" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>    
+    
+    <div class="container px-5" id="PakoSalad">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Pako Salad </h2> 
+            <img src="https://img-global.cpcdn.com/recipes/4dd9930ec29193b0/1200x630cq70/photo.jpg" alt="Pako Salad" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        
+    <div class="container px-5" id="Sisig">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Sisig </h2> 
+            <img src="https://panlasangpinoy.com/wp-content/uploads/2019/12/sizzling-crispy-sisig-recipe-.jpg" alt="Sisig" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="SanNicolasCookies">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> San Nicolas Cookies </h2> 
+            <img src="https://images.summitmedia-digital.com/yummyph/images/2019/07/29/prettysannicolascookies.jpg" alt="SanNicolasCookies" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <div class="container px-5" id="TibukTibuk">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tibuk-Tibuk </h2> 
+            <img src="https://images.summitmedia-digital.com/yummyph/images/2019/06/29/susies-cuisine-tibuk-tibuk-002.jpg" alt="TibukTibuk" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+    <div class="container px-5" id="TokwatBaboy">
+      <div class="row gx-5 align-items-center">
+        <div class="col-lg-12">
+            <br><br>
+            <h2 style="font-family:Verdana, Geneva, Tahoma, sans-serif"> Tokwa't Baboy </h2> 
+            <img src="https://theorchidgardens.com/wp-content/uploads/2017/11/tokwat-baboy.png" alt="TokwatBaboy" class="center">
+            <div class="container p-2">
+        </div>
+
+        <p style= "font-family: Verdana, Geneva, Tahoma, sans-serif;" text-align: center font-size:60%;>
+        Pampanga is proudly the Culinary Capital of the Philippines where great and 
+        astonishing cooks and scrumptious foods are found. In Pampanga, you will savor a wide variety 
+        of dishes from their famous Sisig to their Paella. Exotic dishes will surely tickle your tastebuds from Betute, 
+        or deep fried stuffed frog, to their Kamaru, or mole crickets.
+        Tara! Mangan Tamu!
+        </p>
+        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <?php include "footer.php"; ?>
+</body>
+</html>
--- footer.php
+++ footer.php
@@ -5,19 +5,39 @@
         <div class="footer-col">
           <img src="img/CabalenLight.png" alt="Cabalen Logo" width="395" height="237" class="img">
   	 		</div>
+
   	 		<div class="footer-col">
   	 			<h4>About Us</h4>
           <p style="color:white;">
           Here at Cabalen: Explore Pampanga!
           Our goal is to simply provide you with the best content possible about our hometown, Pampanga. What if you could visit and marvel the famous tourist destinations, scrumptious food and immerse yourself in Kapampangan culture?
-What was once a dream now made reality. “Malagu ka” or should we say, Welcome!</p>
+          What was once a dream now made reality. “Malagu ka” or should we say, Welcome!</p>
+
+        <br>
+        <div class="newsletter">
+            <h4>Subscribe to our Newsletter</h4>
+          </div>
+          
+          
   	 		</div>
   	 		<div class="footer-col">
   	 			<h4>Blogs</h4>
           <p style="color:white;">
           Dive into different cultures, foods, and tourist destinations in Pampanga.
           </p>
-  	 		</div>
+          <br>
+        <div>
+          <form>
+            <div class="container">
+              <input type="text" placeholder="Enter Email address" name="mail" required>
+            </div>
+            <div class="container">
+              <input type="submit" value="Subscribe">
+            </div>
+          </form>
+        </div>
+        </div>
+        <br>
   	 		<div class="footer-col">
   	 			<h4>Pampanga</h4>
            <p style="color:white;">
@@ -31,20 +51,8 @@ What was once a dream now made reality. “Malagu ka” or should we say, Welcom
   	 				<a href="https://twitter.com/CabalenEP"><i class="fab fa-twitter"></i></a>
   	 				<a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
   	 		</div>
-          <br>
           
-          <div class="newsletter">
-            <h4>Subscribe to our Newsletter</h4>
-          </div>
-          
-          <form>
-            <div class="container">
-              <input type="text" placeholder="Enter Email address" name="mail" required>
-            </div>
-            <div class="container">
-              <input type="submit" value="Subscribe">
-            <div>
-          </form>     
+               
   	 		</div>
   	 	</div>
   	 </div>
