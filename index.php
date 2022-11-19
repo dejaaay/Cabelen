91f4a2a1949e316fd9b86bdb4e3f76b1f7217d84
Peter <116708668+PeterJao@users.noreply.github.com>
2022-11-19 11:38:26 +0800

    try

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <!--navbar here -->
+        <!--navbar here 
         <nav class="navbar navbar-expand-sm navbar-dark">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">
@@ -38,6 +38,7 @@
               </div>
             </div>
           </nav>
+          <?php include 'navbar.php' ?>
         <!-- Greetings na naka carousell part -->
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
---
+++ navbar.php
@@ -0,0 +1,24 @@
+
+<nav class="navbar navbar-expand-sm navbar-dark">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="https://raw.githubusercontent.com/dejaaay/Cabelen/main/assets/img/cabalen2.jpg" alt="Cabalen Logo" width="120" height="72">
+  </a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Places</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Videos</a>
+      </li>    
+    </ul>
+  </div>
+</div>
+</nav>
