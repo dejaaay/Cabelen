63c48d2d592b4944a822568057f533bf1cdd4012
qlabrys <qlabrys@gmail.com>
2022-11-19 14:46:31 +0800

    11/19 JM - Changed BG color, BG image, footer

--- videos.php
+++ videos.php
@@ -117,11 +117,11 @@
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6 order-lg-2">
-                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/C8q1F1KHgXE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/VXjrCIcGZmw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                     </div>
                     <div class="col-lg-6 order-lg-1">
                         <div class="p-5">
-                            <h2>Join GMA's Biyahero Drew as he explores internationally-inspired restaurants and attractions in Pampanga.</h2>
+                            <h2>See through a foreign foodie's perspective with this video made by Best Ever Food Review Show.</h2>
                         </div>
                     </div>
                 </div>
@@ -137,15 +137,75 @@
             <div class="container px-5">
                 <div class="row gx-5 align-items-center">
                     <div class="col-lg-6">
-                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/I2zykJTKel4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                        <div class="p-5"><iframe width="500" height="315" src="https://www.youtube.com/embed/0s3XWPti6Rc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                     </div>
                     <div class="col-lg-6">
                         <div class="p-5">
-                            <h2>Watch GMA's Biyahe ni Drew's coverage of the San Guillermo Parish Church in Bacolor, known for it being half-buried due to a lahar overflow during Mount Pinatubo's eruption.</h2>
+                            <h2>A new Night Market just opened in San Fernando! Learn more through this GMA coverage of the Paskuhan Village Night Market.</h2>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
 </body>
+
+<footer class="footer">
+  	 <div class="container">
+     
+  	 	<div class="row">
+        <div class="footer-col">
+          <img src="img/CabalenLight.png" alt="Cabalen Logo" width="395" height="237" class="img">
+  	 		</div>
+
+  	 		<div class="footer-col p-2">
+  	 			<h4>About Us</h4>
+          <p style="color:white;">
+          Here at Cabalen: Explore Pampanga!
+          Our goal is to simply provide you with the best content possible about our hometown, Pampanga. What if you could visit and marvel the famous tourist destinations, scrumptious food and immerse yourself in Kapampangan culture?
+          What was once a dream now made reality. “Malagu ka” or should we say, Welcome!</p>
+
+        <br>
+        <div class="newsletter">
+            <h4>Subscribe to our Newsletter</h4>
+          </div>
+          
+          
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Blogs</h4>
+          <p style="color:white;">
+          Dive into different cultures, foods, and tourist destinations in Pampanga.
+          </p>
+          <br>
+        <div>
+          <form>
+            <div class="container">
+              <input type="text" placeholder="Enter Email address" name="mail" required>
+            </div>
+            <div class="container">
+              <input type="submit" value="Subscribe">
+            </div>
+          </form>
+        </div>
+        </div>
+        <br>
+  	 		<div class="footer-col">
+  	 			<h4>Pampanga</h4>
+           <p style="color:white;">
+           Pampanga is located in the central part of Region III. It is bounded on the north of Tarlac and Nueva Ecija, on the east by Bulacan, southwest by Bataan and west by Zambales. <br>
+           Its terrain is relatively flat with only one distinct mountain, Mount Arayat.
+          </p>
+          <br>
+          <h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="https://www.facebook.com/profile.php?id=100087116093597"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="https://twitter.com/CabalenEP"><i class="fab fa-twitter"></i></a>
+  	 				<a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+  	 		</div>
+          
+               
+  	 		</div>
+  	 	</div>
+  	 </div>
+</footer>
 </html>
